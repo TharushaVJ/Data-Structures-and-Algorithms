aba829c5e8826029208a036bf1034a72d381e689
Tharusha Wijewardena <110379446+TharushaVJ@users.noreply.github.com>
2023-05-20 14:14:27 +0530

    roads and libraries hacker rank problem solved

--- Hackerrank/RoadsAndLibraries.cpp
+++ Hackerrank/RoadsAndLibraries.cpp
@@ -4,66 +4,51 @@
 
 using namespace std;
 
-int minimum(int l, int r){
-    if(l<r) return l;
-    return r;
-}
-
-class HackerLand{
-    int n;
-    vector<list<int>> citiesNear;
-    
-public:
-    HackerLand(int n){
-        this->n=n;
-        citiesNear.resize(n);
-    }
-    void addcity(vector<int> cityNear){
-        citiesNear[cityNear[0]-1].push_back(cityNear[1]);
-        citiesNear[cityNear[1]-1].push_back(cityNear[0]);
-    }
-    void BFS(int s, vector<bool> visited,int c_lib,int c_road,int &l,int &r){
-        list<int> q;
-
-        q.push_back(s);
-        visited[s-1] = true;
-
-        while(!q.empty()){
-            s = q.front();
-            q.pop_front();
-            l+=c_lib;
-            for (int cityNear : citiesNear[s-1]){
-                if (!visited[cityNear-1]){
-                    q.push_back(cityNear);
-                    r+=c_road;
-                    visited[cityNear-1] = true;
-                }
-            }
-            r+=c_lib;
+void DFS(int i, int &r, vector<bool> &visited, vector<vector<int>> &citiesNear){
+    visited[i]=true;
+    for(int city: citiesNear[i]){
+        if(!visited[city]){
+            r++;
+            DFS(city, r, visited, citiesNear);
         }
     }
-};
+}
 
 long roadsAndLibraries(int n, int c_lib, int c_road, vector<vector<int>> cities) {
-    HackerLand H(n);
-    vector<bool> visited(n,false);
-    int l=0;
-    int r=0;
+    //library build cost is less 
+    if (c_lib <= c_road) return n*c_lib;
+
+    //road build cost is less
+    vector<vector<int>> citiesNear(n+1); //adjecancy list format of the graph
+    vector<bool> visited(n+1,false);
+    int r=0; //number of roads 
+    long cost = 0;
     
-    for(vector<int> city : cities) H.addcity(city);
-    
-    for(int i=1; i<=n; i++){
-        if(!visited[i-1]) H.BFS(i,visited,c_lib,c_road,l,r);
+    for(vector<int> city : cities){
+        citiesNear[city[0]].push_back(city[1]);
+        citiesNear[city[1]].push_back(city[0]);
     }
-    return minimum(l,r);
-    
+
+    for(int i=1; i<=n; i++){
+        if(!visited[i]){
+            //calculate cost for a connected segment of the graph
+            DFS(i, r, visited, citiesNear);
+            cost += r*c_road + c_lib;
+            r = 0; //reset r to 0
+        }
+    }
+    return cost;    
 }
 
 int main(){
-    int n = 3;
-    int c_lib = 2;
-    int c_road = 1;
-    vector<vector<int>> cities {{1, 2}, {3, 1}, {2, 3}};
+    int n = 6;
+    int c_lib = 5;
+    int c_road = 2;
+    vector<vector<int>> cities {{1, 3}, {3, 4}, {2, 4}, {1, 2}, {2, 3}, {5, 6}};
+    //vector<vector<int>> cities {{1, 2}, {3, 4}, {5, 6},{7, 2}, {7, 4}, {7, 6}};
+    //vector<vector<int>> cities {{1, 2}, {1, 3}, {1, 4}};
+    //vector<vector<int>> cities {};
+
 
     cout<<roadsAndLibraries(n, c_lib, c_road, cities)<<endl;
 }
